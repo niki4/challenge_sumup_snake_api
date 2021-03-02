8dafa035c133e1eed8e34e1ab8f4ad02ae1ae7ba
niki4 <nikiforr@gmail.com>
2021-03-02 18:08:41 +0300

    Parse width & height in query for newGameHandler

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -13,7 +15,48 @@ func newGameHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	_, _ = fmt.Fprintln(w, "newGameHandler response OK")
+
+	queryParams := r.URL.Query()
+	widths, widthPresent := queryParams["w"]
+	heights, heightPresent := queryParams["h"]
+
+	if !widthPresent {
+		http.Error(w, "'width' missed in request", http.StatusBadRequest)
+		return
+	}
+	width, err := strconv.Atoi(widths[0])
+	if err != nil {
+		http.Error(w, "'width' is not an integer number", http.StatusBadRequest)
+		return
+	}
+	if width <= 0 {
+		http.Error(w, "'width' cannot be zero or negative number", http.StatusBadRequest)
+		return
+	}
+
+	if !heightPresent {
+		http.Error(w, "'height' missed in request", http.StatusBadRequest)
+		return
+	}
+	height, err := strconv.Atoi(heights[0])
+	if err != nil {
+		http.Error(w, "'height' is not an integer number", http.StatusBadRequest)
+		return
+	}
+	if height <= 0 {
+		http.Error(w, "'height' cannot be zero or negative number", http.StatusBadRequest)
+		return
+	}
+
+	newGameState := state{
+		Width:  width,
+		Height: height,
+	}
+
+	if err = json.NewEncoder(w).Encode(newGameState); err != nil {
+		http.Error(w, "error on encoding JSON response: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // validateGameHandler handles POST requests to process game steps
